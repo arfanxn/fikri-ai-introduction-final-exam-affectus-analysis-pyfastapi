f167e08c7a971c30c6327db240530d1c59857774
arfanxn <arfan2173@gmail.com>
2024-06-16 00:40:46 +0700

    update main file

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/api/sentiment-analyses")
 async def get_sentiment_analysis():
-    content = json.dumps({"message": "This url is on an under construction"})
+    content = json.dumps({"message": "This url is on under construction"})
     return Response(status_code=status.HTTP_200_OK, content=content, media_type="application/json")
 
 @app.post("/api/sentiment-analyses")
@@ -15,7 +15,7 @@ async def store_sentiment_analysis(text: Annotated[str, Form()]):
     service = SentimentAnalysisService()
     analyzed_sentiments = service.predict([text])
     content = json.dumps({
-        "message": "Successfully analyzed sentiments.",
+        "message": "Successfully analyzed sentiment(s).",
         "analyzed_sentiment" : analyzed_sentiments[0]
     })
     return Response(status_code=status.HTTP_200_OK, content=content, media_type="application/json")
